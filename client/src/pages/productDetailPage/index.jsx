f859b41cecd123a2c991a64c0240a29abecc971f
Elxan <elxan.offical.07@mail.com>
2024-11-20 19:53:53 +0400

    demo

--- client/src/pages/productDetailPage/index.jsx
+++ client/src/pages/productDetailPage/index.jsx
@@ -53,9 +53,9 @@ const ProductDetailPage = () => {
           <img
             className="w-full h-full object-cover transition-all duration-300 hover:scale-105 cursor-pointer "
             src={product.img}
-            alt=""
           />
         </div>
+
 
         <div className=" w-2/4 flex flex-col gap-5 items-start">
           <h2 className="text-white text-3xl font-semibold tracking-wider">
